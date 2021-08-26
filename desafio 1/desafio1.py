0ec2cc116e0bbe9967b7483690d590b049d2f7c8
Joel Júnior <joel.j@escolar.ifrn.edu.br>
2021-08-26 15:59:54 -0300

    Comentários

--- desafio 1/desafio1.py
+++ desafio 1/desafio1.py
@@ -1,16 +1,16 @@
-# Função que irá calcular o número de passos até que
-# N volte para 1
+# função que irá calcular o número de passos até que
+# n volte para 1
 def num_de_passos(n, dicionario_passos):
     num_passos = 0
     n_temp = n
 
-    # Uso do while True ara que o loop seja executado pelo menos 1 vez
+    # uso do while true ara que o loop seja executado pelo menos 1 vez
     while True:
-        # Verifica se já existe um número de passos registrado para o
-        # valor de N atual
+        # verifica se já existe um número de passos registrado para o
+        # valor de n atual
         passos_temp = dicionario_passos.get(n_temp)
 
-        # Caso exista, armazena na varíavel que será retornada
+        # caso exista, armazena na varíavel que será retornada
         if passos_temp is not None:
             num_passos += passos_temp
             break
@@ -20,28 +20,26 @@ def num_de_passos(n, dicionario_passos):
             n_temp = n_temp / 2
 
         else:
-            # Aumenta a contagem de passos em 2, porque estão sendo feitos
+            # aumenta a contagem de passos em 2, porque estão sendo feitos
             # 2 passos na mesma fórmula
             num_passos += 2
-            # Todo número n, ímpar, quando aplicado à fórmula 3n + 1
+            # todo número n, ímpar, quando aplicado à fórmula 3n + 1
             # torna-se um número par, então podemos fazer 2 passos em 1
-            n_temp = (3*n_temp + 1)//2
+            n_temp = (3*n_temp + 1)/2
 
-        # Parar o loop em n = 1, depois de todas as contagens de passos,
+        # parar o loop em n = 1, depois de todas as contagens de passos,
         # para que não entre em loop infinito
         if n_temp == 1:
             break
 
-    # Registra o número de passos para N no dicionário
+    # registra o número de passos para n no dicionário
     dicionario_passos[n] = num_passos
     return num_passos
 
 
 def main():
-    # Armazena o maior número de passos para voltar a 1
-    maior_num_passos = 0
-    # Armazena o valor que gera tal número de passos
-    maior_n = 0
+    maior_num_passos = 0    # maior número de passos para voltar a 1
+    maior_n = 0             # valor que gera tal número de passos
 
     # dicionário para armazenar o número de passos gerados por cada número
     # para evitar repetição de verificações por números já consultados
