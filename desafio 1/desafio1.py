cc966ee6970a48cc07c3988b437a07756fd18fbc
Joel Júnior <joel.j@escolar.ifrn.edu.br>
2021-08-26 12:53:36 -0300

    Otimização na contagem de passos

--- desafio 1/desafio1.py
+++ desafio 1/desafio1.py
@@ -11,7 +11,9 @@ def num_de_passos(n):
 
         else:
             num_passos += 1
-            n = 3*n + 1
+            # Todo número n, ímpar, quando aplicado à fórmula 3n + 1
+            # torna-se um número par, então podemos fazer 2 passos em 1
+            n = (3*n + 1)//2
 
         # Parar o loop em n = 1, depois de todas as contagens de passos,
         # para que não entre em loop infinito
