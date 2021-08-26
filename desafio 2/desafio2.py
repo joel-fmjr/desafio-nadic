796e7aeff39a9278495f78c93bd2ecd6e8dd894c
Joel Júnior <joel.j@escolar.ifrn.edu.br>
2021-08-26 20:35:51 -0300

    Desafio 2

--- desafio 2/desafio2.py
+++ desafio 2/desafio2.py
@@ -5,6 +5,7 @@ texto1 = "Olá meu nome é Julia Fernandes"
 texto2 = "Olá me chamo Marcelo Marcondes"
 texto3 = "Olá meu nome é F3rnando Fernandes"
 
+# compilar a expressão, já que ela será usada mais de 1 vez
 regex = re.compile(r'([A-Z][a-z]+) ([A-Z][a-z]+)')
 
 print(regex.findall(texto1))
